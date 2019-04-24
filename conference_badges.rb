6bd7da83a8422d61c0ff5bf47e38d6fd0ee8994a
Chris Kay <chriskay25@gmail.com>
2019-04-24 17:09:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,11 @@ def batch_badge_creator(attendees)
     badge_maker(name)
   end
 end 
+
+def assign_rooms(attendees)
+  room = 0 
+  attendees.each do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end 
+end
